00bec73b68a10f498663ae0de5d17dd55ab564f8
Bjørn Einar <bjartnes@gmail.com>
2013-09-08 21:54:02 -0700

    Only need js interop back again. and event.s

--- CljsTest/channels/project.clj
+++ CljsTest/channels/project.clj
@@ -3,8 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+				 [prismatic/dommy "0.1.1"]
+				 [org.clojure/core.async "0.1.0-SNAPSHOT"]]
   :plugins [[lein-cljsbuild "0.3.2"]]
+  :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
   :cljsbuild {:builds [{:source-paths ["src"]
                         :compiler {:output-to "..\\js\\channels.js"
 								   :optimizations :simple
--- CljsTest/channels/src/channel.cljs
+++ CljsTest/channels/src/channel.cljs
@@ -1,3 +1,20 @@
-(ns channel)
+(ns channel
+  (:use-macros [dommy.macros :only [sel1]])
+  (:require [dommy.core] [cljs.core.async :as async :refer [<! >! chan close! timeout]])
+  (:require-macros [cljs.core.async.macros :as m :refer [go alt!]]))
 (defn greet [n]
   (str "Hello you" n))
+
+
+(defn test []
+  (let [beb (chan)]
+      (go
+       (loop [x 1]
+           (<! (timeout 200))
+           (>! beb "hello")
+		   (.log js/console x)
+           (recur (+ x 1))))
+      (go
+       (loop [x 1]
+			(.log js/console (<! beb))
+			(recur (+ x 1))))))
